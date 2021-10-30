c1fd9ea6cb185628a4c1f949a339984240b3385f
mohammad-alsharif <mohammad.alsharif.med.s@tuwaiq.edu.sa>
2021-10-30 21:24:47 +0300

    Add UIViewController Add  UITabBarController, UITabBar and UITabBarItem Add  UINavigationController, UINavigationBar and UINavigationItem Add  UITableViewController, UITableView and UITableViewCell

---
+++ Project2/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  Project2
+//
+//  Created by mohammad.alsharif on 24/03/1443 AH.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var List: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
